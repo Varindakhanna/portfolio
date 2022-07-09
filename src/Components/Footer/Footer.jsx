b5f880b42745f7e933f20895ad42afb0112966d8
VARINDA KHANNA <varindakhanna2002@gmail.com>
2022-07-09 11:12:13 +0530

    footer component added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import {FaFacebookF,FaInstagram ,FaTwitter} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <section id='footer'>
-      Footer it is
-    </section>
-  )
-}
+    <section id="footer">
+      <footer>
+        <a href="#" className="footer__logo">EGATOR</a>
 
-export default Footer
+        <ul className="permalinks">
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
+
+        </ul>
+
+        <div className="footer__socials">
+          <a href="htpps://facebook.com"><FaFacebookF/></a>
+          <a href="htpps://instagram.com"><FaInstagram/></a>
+          <a href="htpps://twitter.com"><FaTwitter/></a>
+
+        </div>
+
+        <div className="footer__copyright">
+          <small>&copy; EGATOR Tutorials . All rights reserved</small>
+
+        </div>
+      </footer>
+    </section>
+  );
+};
+
+export default Footer;
